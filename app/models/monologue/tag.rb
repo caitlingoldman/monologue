cc4ac244e5ef2f9f77f8acb5d4c0024135a1b21b
Cesar Camacho <ccamacho@gapintelligence.com>
2015-03-23 00:37:41 -0700

    Adds scopes for frequency counts and published posts tags

--- app/models/monologue/tag.rb
+++ app/models/monologue/tag.rb
@@ -4,11 +4,20 @@ class Monologue::Tag < ActiveRecord::Base
 
   validates :name, uniqueness: true, presence: true
 
+  scope :for_published_posts, -> { published_posts_join.uniq }
+  scope :max_frequency, -> { published_posts_join.group("monologue_tags.id").order("count_all ASC").count.values.max }
+  scope :min_frequency, -> { published_posts_join.group("monologue_tags.id").order("count_all ASC").count.values.min }
+
   def posts_with_tag
     self.posts.published
   end
 
   def frequency
     posts_with_tag.size
+  end
+
+  private
+  def self.published_posts_join
+    self.joins(:posts).where(monologue_posts: { published: true }).where("monologue_posts.published_at <= ?", DateTime.now)
   end
 end
